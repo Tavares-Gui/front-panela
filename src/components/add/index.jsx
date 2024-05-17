a8f94acc2058a0fb3f9fdc62556b66a576832929
Guilherme Rodrigues Tavares <grtppg@gmail.com>
2024-05-17 10:54:24 -0300

    add

--- src/components/add/index.jsx
+++ src/components/add/index.jsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 
 import { Backdrop, Box, Modal, Fade, Typography } from '@mui/material';
 
-const style = {
+import BtnAdd from '../../assets/icon/botao-adicionar.png'
+
+const styleModal = {
     position: 'absolute',
     top: '50%',
     left: '50%',
@@ -12,6 +14,60 @@ const style = {
     borderRadius: '20px',
     boxShadow: 24,
     p: 4,
+    textAlign: 'center',
+    overflowY: 'auto',
+    maxHeight: '90vh'
+};
+
+const styleInput = {
+    width: 200,
+    height: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    border: 'none',
+    borderRadius: '20px',
+    backgroundColor: '#D9D9D9'
+};
+
+const styleInputQtd = {
+    height: 30,
+    width: 60,
+    paddingLeft: 10,
+    paddingRight: 10,
+    border: 'none',
+    borderRadius: '50px',
+    backgroundColor: '#D9D9D9',
+};
+
+const styleTextDesc = {
+    height: 80,
+    width: 400,
+    paddingLeft: 10,
+    paddingRight: 10,
+    border: 'none',
+    borderRadius: '20px',
+    backgroundColor: '#D9D9D9',
+};
+
+const styleDiv = {
+    display: 'flex',
+    justifyContent: 'space-between'
+};
+
+const styleBtn = {
+    button: {
+        marginTop: 10,
+        height: '48px',
+        width: '48px',
+        border: 'none',
+        background: 'none',
+        padding: 0,
+        transition: 'transform 0.8s',
+        cursor: 'pointer',
+    },
+    buttonHover: {
+        transform: 'scale(1.1)',
+    }
 };
 
 export default function AddModal({ open, handleClose }) {
@@ -28,28 +84,58 @@ export default function AddModal({ open, handleClose }) {
             }}
         >
             <Fade in={open}>
-                <Box sx={{ ...style, textAlign: 'center', overflowY: 'auto', maxHeight: '90vh' }}>
-                    <Typography variant="h3" id="transition-modal-title" gutterBottom>
+                <Box sx={{ ...styleModal }}>
+                    <Typography variant="h4" id="transition-modal-title" gutterBottom>
+                        Adicionar Receita
+                    </Typography>
+                    <Typography variant="h6" id="transition-modal-description" gutterBottom>
                         Nome
                     </Typography>
-                    <Typography variant="h5" id="transition-modal-description" gutterBottom>
+                    <input type='text' style={{ ...styleInput }}></input>
+                    <Typography variant="h6" id="transition-modal-description" gutterBottom>
                         Descrição
                     </Typography>
-                    <Typography variant="body1" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
-                    <Typography variant="h5" gutterBottom>
-                        Ingredientes
-                    </Typography>
-                    <ul style={{ textAlign: 'left', listStylePosition: 'inside' }}>
-                        <li>Ingrediente 1</li>
-                    </ul>
-                    <Typography variant="h5" gutterBottom>
-                        Modo de Preparo
-                    </Typography>
-                    <ul style={{ textAlign: 'left', listStylePosition: 'inside' }}>
-                        <li>Passo 1</li>
-                    </ul>
+                    <textarea style={{ ...styleTextDesc }}></textarea>
+                    <div style={{ ...styleDiv }}>
+                        <div>
+                            <Typography variant="h6" id="transition-modal-description" gutterBottom>
+                                Qtd.
+                            </Typography>
+                            <input type='number' style={{ ...styleInputQtd }}></input>
+                        </div>
+                        <div>
+                            <Typography variant="h6" id="transition-modal-description" gutterBottom>
+                                Ingrediente
+                            </Typography>
+                            <input type='text' style={{ ...styleInput }}></input>
+                        </div>
+                        <div>
+                            <Typography variant="h6" id="transition-modal-description" gutterBottom>
+                                Modo de Preparo
+                            </Typography>
+                            <input type='text' style={{ ...styleInput }}></input>
+                        </div>
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                        <div>
+                            <button
+                                style={styleBtn.button}
+                                onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.1)'}
+                                onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                            >
+                                <img src={BtnAdd} alt="Botão Adicionar" style={{ height: '100%', width: '100%' }} />
+                            </button>
+                        </div>
+                        <div>
+                            <button
+                                style={styleBtn.button}
+                                onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.1)'}
+                                onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                            >
+                                <img src={BtnAdd} alt="Botão Adicionar" style={{ height: '100%', width: '100%' }} />
+                            </button>
+                        </div>
+                    </div>
                 </Box>
             </Fade>
         </Modal>
